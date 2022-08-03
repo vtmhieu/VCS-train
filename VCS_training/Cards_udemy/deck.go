1b219923033718d58e2af0198cf63472b3353cb7
vtmhieu <88451173+vtmhieu@users.noreply.github.com>
2022-08-04 00:37:08 +0700

    Add files via upload

---
+++ VCS_training/Cards_udemy/deck.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+//create a new type of 'deck'
+//which is a slice of strings
+
+type deck []string
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
+
+//21
