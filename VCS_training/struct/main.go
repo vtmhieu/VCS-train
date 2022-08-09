d94d1bc9b3b736208543e07ea3a06f7f40e7beed
vtmhieu <vtmhieu111@gmail.com>
2022-08-09 16:22:47 +0700

    d

---
+++ VCS_training/struct/main.go
@@ -0,0 +1,54 @@
+package main
+
+import "fmt"
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+type person struct {
+	firstName string
+	lastName  string
+	contact   contactInfo
+}
+
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
+}
+
+//  &variable give me the memory address of the value this variable is pointing at
+// *pointer give me the value this memory address is pointing at
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
+
+func main() {
+	//tell go what different fields of a person/struct should have
+	//create a new value of type person
+
+	//1st way
+
+	// alex := person{"Hieu", "Vu"}
+	// fmt.Println(alex)
+
+	//2nd way
+	// var alex person
+	// alex.firstName = "Hieu"
+	// alex.lastName = "Vu"
+	// fmt.Println(alex)
+	// fmt.Printf("%+v", alex)
+
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Hieu",
+		contact: contactInfo{
+			email:   "hieu@gmail.com",
+			zipCode: 23323,
+		},
+	}
+
+	jimPointer := &jim
+
+	jimPointer.updateName("Hie")
+	jim.print()
+}
