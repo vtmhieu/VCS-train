cb7e0674a6155fd25e4299ef2fd66057cbfa7e72
vtmhieu <vtmhieu111@gmail.com>
2022-08-09 14:40:54 +0700

    k

--- VCS_training/Cards_udemy/deck_test.go
+++ VCS_training/Cards_udemy/deck_test.go
@@ -1,9 +1,19 @@
 package main
 
+import "testing"
+
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
 
 	if len(d) != 16 {
-		t.Error("Expected deck length of 20, but got ", len(d))
+		t.Errorf("Expected deck length of 16, but got %v", lend(d))
+	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected ace of Spades, but got %v", d[0])
+	}
+
+	if d[len(d)-1] != "Ace of Spades" {
+		t.Errorf("Expected ace of Spades, but got %v", d[len(d)-1])
 	}
 }
