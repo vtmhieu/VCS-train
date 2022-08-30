d6be5aa0fed711919a81aa06353c5aed4cb892b8
vtmhieu <vtmhieu111@gmail.com>
2022-08-30 17:54:22 +0700

    d

---
+++ VCS_training/homework/homework1/main.go
@@ -0,0 +1,29 @@
+package main 
+
+
+
+type customer struct{
+	name string 
+	email string
+	sdt int64
+	code int64
+	type_room string
+	description string
+}
+
+customers := []customer 
+
+
+type room struct{
+	code_room int64
+	type_room string 
+	price_room int64
+}
+
+func getInfomation
+
+func main(){
+	fmt.Println("Welcome to our hotel!")
+	fmt.Println("Please enter your name and email address:")
+
+}
