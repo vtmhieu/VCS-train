802e8426b6c4fdf5bc11737757d4b88a28a22b42
vtmhieu <vtmhieu111@gmail.com>
2022-09-05 13:56:50 +0700

    d

--- VCS_training/homework/homework1/main.go
+++ VCS_training/homework/homework1/main.go
@@ -19,33 +19,38 @@ type room struct {
 
 type customers []customer
 
-func (Customers customer) getInfomation() []customer {
-	cus := []customer{}
+func getInfomation() customer {
+	var cus customer
+
 	fmt.Println("Please fill in this form")
 	fmt.Print("Name? ")
-	fmt.Scan(&cus[len(cus)].name)
+	fmt.Scan(&cus.name)
 	fmt.Print("email? ")
-	fmt.Scan(&cus[len(cus)].email)
+	fmt.Scan(&cus.email)
 	fmt.Print("sdt? ")
-	fmt.Scan(&cus[len(cus)].sdt)
+	fmt.Scan(&cus.sdt)
 	fmt.Print("type_room? ")
-	fmt.Scan(&cus[len(cus)].type_room)
+	fmt.Scan(&cus.type_room)
 	fmt.Print("description? ")
-	fmt.Scan(&cus[len(cus)].description)
+	fmt.Scan(&cus.description)
 
 	return cus
 }
 
-func (Customers customer) PrintInfomation() {
-
+func (c customer) PrintInfomation() {
+	fmt.Println("Name: ", c.name)
+	fmt.Println(c.name, "'s email: ", c.email)
+	fmt.Println(c.name, "'s phone number: ", c.sdt)
+	fmt.Println(c.name, "'s type room: ", c.type_room)
+	fmt.Println("Description: ", c.description)
 }
 
 func main() {
 	Customers := []customer{}
-	fmt.Println("Welcome to our hotel!")
-	fmt.Println("Please enter your name and email address:")
 
-	for _, v := range Customers {
-		Customers = append(Customers, Customers.getInfomation())
+	Customers = append(Customers, getInfomation())
+	for _, c := range Customers {
+		fmt.Println("This is the informations of customer ", c.name)
+		c.PrintInfomation()
 	}
 }
