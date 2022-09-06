fbde6a15d4d095210e74d1bcee95eaeb649a1ed5
vtmhieu <vtmhieu111@gmail.com>
2022-09-06 14:48:02 +0700

    d

--- VCS_training/homework/homework1/main.go
+++ VCS_training/homework/homework1/main.go
@@ -1,6 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+)
 
 type customer struct {
 	name        string `json:"name"`
@@ -11,73 +18,162 @@ type customer struct {
 	description string `json:"description"`
 }
 
-type customers struct {
-	customers []customer `json:"customers"`
-}
+// type customers struct {
+// 	customers []customer `json:"customers"`
+// }
 
 type room struct {
-	code_room  int64
-	type_room  string
-	price_room int64
+	id_room    int    `json:"room_id"`
+	type_room  string `json:"room_type"`
+	price_room int    `json:"room_price"`
 }
 
-func get_cus_Infomation() customer {
-	var cus customer
+func nextLine() string {
+	bio := bufio.NewReader(os.Stdin)
+	line, _, _ := bio.ReadLine()
+	return string(line)
+}
+func ReadFile(filename string) []byte {
+	content, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return content
+}
 
-	fmt.Println("Please fill in this form")
-	fmt.Print("Name? ")
-	fmt.Scan(&cus.name)
-	fmt.Print("email? ")
-	fmt.Scan(&cus.email)
-	fmt.Print("sdt? ")
-	fmt.Scan(&cus.sdt)
-	fmt.Print("type_room? ")
-	fmt.Scan(&cus.type_room)
-	fmt.Print("description? ")
-	fmt.Scan(&cus.description)
+func SaveRoom(filename string, rooms []room) {
+	data, err := json.MarshalIndent(rooms, "", " ")
+	if err != nil {
+		fmt.Println(err)
+	}
 
+	err = ioutil.WriteFile(filename, []byte(data), 0666)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+}
+
+func LoadRoom(filename string) []room {
+
+	var rooms []room
+	data := ReadFile(filename)
+	_ = json.Unmarshal(data, &rooms)
+	return rooms
+}
+
+func SaveCus(filename string, customers []customer) {
+	data, err := json.MarshalIndent(customers, "", " ")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	err = ioutil.WriteFile(filename, []byte(data), 0666)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func LoadCus(filename string) []customer {
+	var cus []customer
+
+	data := ReadFile(filename)
+	_ = json.Unmarshal(data, &cus)
 	return cus
 }
 
-func (c customer) Print_cus_Infomation() {
-	fmt.Println("Name: ", c.name)
-	fmt.Println(c.name, "'s email: ", c.email)
-	fmt.Println(c.name, "'s phone number: ", c.sdt)
-	fmt.Println(c.name, "'s type room: ", c.type_room)
-	fmt.Println("Description: ", c.description)
-}
-
-func SavetoFile() {
-
-}
-
-func main() {
-	Customers := []customer{}
-
-	var n int
-	fmt.Println("----------------------------------------")
-	fmt.Println("MENU")
-	fmt.Println("1.Enter new room")
-	fmt.Println("2.Enter new customer")
-	fmt.Println("3.Book")
-	fmt.Println("4.Sort room booking by room type")
-	fmt.Println("5.Create bill for customer")
-	fmt.Println("6.Out program")
-	fmt.Print("Your choice? ")
-	fmt.Scan(&n)
-
-	switch n {
-	case 1:
-	case 2:
-		Customers = append(Customers, get_cus_Infomation())
-
-		break
-	case 3:
-	case 4:
-	case 5:
-	case 6:
-		return
-	default:
-		main()
+func InsertRoomID(Newroom *room, err error) int {
+	fmt.Print("ID: ")
+	Newroom.id_room, err = strconv.Atoi(nextLine())
+	if err != nil {
+		fmt.Println("The input must be integer")
+		fmt.Println("Please reinsert the room ID ")
+		InsertRoomID(Newroom, err)
 	}
+	if Newroom.id_room < 0 {
+		fmt.Println("The input must be positive number")
+		fmt.Println("Please reinsert the room ID ")
+		InsertRoomID(Newroom, err)
+	}
+	return Newroom.id_room
+}
+
+func InsertRoomType(Newroom *room, err error) string {
+	fmt.Print("1. Single  2. Double  3. President ? ")
+	var k int
+	fmt.Scanln(&k)
+
+	switch k {
+	case 1:
+		Newroom.type_room = "Single"
+
+	case 2:
+		Newroom.type_room = "Double"
+
+	case 3:
+		Newroom.type_room = "President"
+
+	default:
+		fmt.Println("Please enter room type in range 1 to 3")
+		InsertRoomType(Newroom, err)
+	}
+	return Newroom.type_room
+}
+func main() {
+
+	var rooms []room
+	var err error
+	rooms = LoadRoom("Phong.json")
+	for {
+		var n int
+		fmt.Println("----------------------------------------")
+		fmt.Println("MENU")
+		fmt.Println("1. Enter new room")
+		fmt.Println("2. Enter new customer")
+		fmt.Println("3. Book")
+		fmt.Println("4. Sort room booking by room type")
+		fmt.Println("5. Create bill for customer")
+		fmt.Println("6. Out program")
+		fmt.Println("Your choice? ")
+		fmt.Scanln(&n)
+
+		switch n {
+		case 1:
+
+			fmt.Print("Pleas enter room infomation:\n")
+			var Room room
+			InsertRoomID(&Room, err)
+
+			fmt.Println("Type of room: ")
+			InsertRoomType(&Room, err)
+
+			fmt.Println("Price of room: ")
+			Room.price_room, err = strconv.Atoi(nextLine())
+			if err != nil {
+				fmt.Println("Input must be number")
+				break
+			}
+			if Room.price_room < 0 {
+				fmt.Println("Input must be positive number")
+				break
+			}
+
+			rooms = append(rooms, Room)
+			SaveRoom("Phong.json", rooms)
+			fmt.Printf("%10s%10s%10s\n", "ID", "Type", "Price")
+			fmt.Printf("%10d%10s%10d\n", *&Room.id_room, *&Room.type_room, *&Room.price_room)
+
+		case 2:
+
+			break
+		case 3:
+		case 4:
+		case 5:
+		case 6:
+			return
+		default:
+
+		}
+	}
+
 }
