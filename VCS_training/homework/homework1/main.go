efb84d458b2d7090ba421846011bc9fe30e9d7b4
vtmhieu <vtmhieu111@gmail.com>
2022-09-07 01:09:46 +0700

    d

--- VCS_training/homework/homework1/main.go
+++ VCS_training/homework/homework1/main.go
@@ -10,12 +10,10 @@ import (
 )
 
 type customer struct {
-	Name        string `json:"name"`
-	Email       string `json:"email"`
-	Sdt         int64  `json:"sdt"`
-	Code        int64  `json:"code"`
-	Type_room   string `json:"type_room"`
-	Description string `json:"description"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+	Sdt   int    `json:"sdt"`
+	ID    int    `json:"id"`
 }
 
 // type customers struct {
@@ -144,10 +142,78 @@ func InsertRoomPrice(Newroom *room, err error) int {
 	}
 	return Newroom.Price_room
 }
-func main() {
 
+func InsertCusName(Newcustomer *customer) string {
+	fmt.Println("Name: ")
+	Newcustomer.Name = nextLine()
+	if Newcustomer.Name == "" {
+		fmt.Println("Please reinsert customer name.")
+		InsertCusName(Newcustomer)
+	}
+	return Newcustomer.Name
+}
+
+func InsertCusEmail(Newcustomer *customer) string {
+	fmt.Println("Email:")
+	Newcustomer.Email = nextLine()
+	if len(Newcustomer.Email) <= 10 || Newcustomer.Email[(len(Newcustomer.Email)-10):(len(Newcustomer.Email))] != ("@gmail.com") {
+		fmt.Println("Please reinsert customer email.")
+		fmt.Println("The email must have the tail of @gmail.com")
+		InsertCusEmail(Newcustomer)
+	}
+	return Newcustomer.Email
+}
+
+func InsertCusPhone(Newcustomer *customer, err error) int {
+	fmt.Println("Phone number:")
+	Newcustomer.Sdt, err = strconv.Atoi(nextLine())
+	if err != nil {
+		fmt.Println("The input must be number.")
+		fmt.Println("Please reinsert customer phone.")
+		InsertCusPhone(Newcustomer, err)
+	}
+	if Newcustomer.Sdt < 0 || len(strconv.Itoa(Newcustomer.Sdt)) != 8 {
+		fmt.Println("Wrong input. Input must have 8 numbers")
+		fmt.Println("Please reinsert customer phone.")
+		InsertCusPhone(Newcustomer, err)
+	}
+	return Newcustomer.Sdt
+}
+
+func InsertCusID(Newcustomer *customer, err error, customers []customer) int {
+	fmt.Println("ID:")
+	Newcustomer.ID, err = strconv.Atoi(nextLine())
+	if err != nil {
+		fmt.Println("The input must be integer")
+		fmt.Println("Please reinsert the room ID.")
+		InsertCusID(Newcustomer, err, customers)
+	}
+	if Newcustomer.ID < 0 {
+		fmt.Println("The input must be positive number")
+		fmt.Println("Please reinsert the room ID ")
+		InsertCusID(Newcustomer, err, customers)
+	}
+	n := 0
+	for _, v := range customers {
+		if Newcustomer.ID == v.ID {
+			n++
+		}
+	}
+	if n > 0 {
+		fmt.Println("The ID has been used. Please reinsert the room ID.")
+		InsertCusID(Newcustomer, err, customers)
+	}
+	if n == 0 {
+		return Newcustomer.ID
+	}
+	return Newcustomer.ID
+}
+
+func main() {
+	var customers []customer
 	var rooms []room
 	var err error
+	customers = LoadCus("customer.json")
 	rooms = LoadRoom("room.json")
 	for {
 		var n int
@@ -182,6 +248,20 @@ func main() {
 			fmt.Printf("%10d%10s%10d\n", Newroom.Id_room, Newroom.Type_room, Newroom.Price_room)
 
 		case 2:
+
+			fmt.Print("Please enter customer infomation:\n")
+			var Newcus customer
+			InsertCusName(&Newcus)
+
+			InsertCusEmail(&Newcus)
+
+			InsertCusPhone(&Newcus, err)
+
+			InsertCusID(&Newcus, err, customers)
+
+			fmt.Println("Successful!")
+			customers = append(customers, Newcus)
+			SaveCus("customer.json", customers)
 
 		case 3:
 		case 4:
