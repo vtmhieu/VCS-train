c88409e926e65bbf80b7cf4f1438255008edb6a2
vtmhieu <vtmhieu111@gmail.com>
2022-09-15 00:26:49 +0700

    d

--- gin test/main.go
+++ gin test/main.go
@@ -1,13 +1,44 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+	"time"
 
+	"github.com/gin-gonic/gin"
+	"github.com/rs/xid"
+)
+
+type Recipe struct {
+	ID           string    `json:"id"`
+	Name         string    `json:"name"`
+	Tags         []string  `json:"tags"`
+	Ingredents   []string  `json:"ingredents"`
+	Instructions []string  `json:"instructions"`
+	PublishedAt  time.Time `json:"published_at"`
+}
+
+var recipes []Recipe
+
+func init() {
+	recipes = make([]Recipe, 0)
+}
+
+//POST /recipes
+func NewRecipeHandler(c *gin.Context) {
+	var recipe Recipe
+	if err := c.ShouldBindJSON(&recipe); err != nil {
+
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error()})
+		return
+	}
+	recipe.ID = xid.New().String()
+	recipe.PublishedAt = time.Now()
+	recipes = append(recipes, recipe)
+	c.JSON(http.StatusOK, recipe)
+}
 func main() {
 	router := gin.Default()
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "hello world",
-		})
-	})
+	router.POST("/recipes", NewRecipeHandler)
 	router.Run()
 }
