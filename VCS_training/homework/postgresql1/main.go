b76aad0be9dcdea079515e489beb822f207cb01b
vtmhieu <vtmhieu111@gmail.com>
2022-09-16 14:32:53 +0700

    v

--- VCS_training/homework/postgresql1/main.go
+++ VCS_training/homework/postgresql1/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"database/sql"
-	"encoding/json"
 	"fmt"
 
 	_ "github.com/lib/pq"
@@ -13,9 +12,57 @@ const (
 	port     = 5432
 	user     = "postgres"
 	password = "1"
-	dbname   = "VCS_cycir"
+	dbname   = "VCS-cycir"
 )
 
+func connectDB() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+}
+func CreateBlankTable(table string) {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	sqlStatement := `
+	CREATE TABLE $1
+	`
+	err = db.(sqlStatement, table)
+	if err != nil{
+		panic(err)
+	}
+}
+func AddColumn() {
+	sqlStatement := `
+	ALTER TABLE $1
+	ADD COLUMN $2 
+	`
+	_, err := db.Exec(sqlStatement, 1, "NewFirst", "NewLast")
+	if err != nil {
+		panic(err)
+	}
+}
 func main() {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
@@ -32,54 +79,4 @@ func main() {
 	}
 
 	fmt.Println("Successfully connected!")
-}
-
-var members []member
-
-type member struct {
-	username  string `json:"username"`
-	firstname string `json:"firstname"`
-	lastname  string `json:"lastname"`
-	group_id  int    `json:"group_id"`
-}
-
-func getJSON(sqlString string) (string, error) {
-	rows, err := db.Query(sqlString)
-	if err != nil {
-		return "", err
-	}
-	defer rows.Close()
-	columns, err := rows.Columns()
-	if err != nil {
-		return "", err
-	}
-	count := len(columns)
-	tableData := make([]map[string]interface{}, 0)
-	values := make([]interface{}, count)
-	valuePtrs := make([]interface{}, count)
-	for rows.Next() {
-		for i := 0; i < count; i++ {
-			valuePtrs[i] = &values[i]
-		}
-		rows.Scan(valuePtrs...)
-		entry := make(map[string]interface{})
-		for i, col := range columns {
-			var v interface{}
-			val := values[i]
-			b, ok := val.([]byte)
-			if ok {
-				v = string(b)
-			} else {
-				v = val
-			}
-			entry[col] = v
-		}
-		tableData = append(tableData, entry)
-	}
-	jsonData, err := json.Marshal(tableData)
-	if err != nil {
-		return "", err
-	}
-	fmt.Println(string(jsonData))
-	return string(jsonData), nil
 }
